2700f45a3cb243e2e745ce1a405f43aa03913518
akjayant <ashish000987@gmail.com>
2017-03-24 12:46:37 +0530

    Update equations.py

--- assignment2_Algorrithm/equations.py
+++ assignment2_Algorrithm/equations.py
@@ -7,14 +7,14 @@ Created on Fri Mar 24 09:21:21 2017
 """
 from __future__ import division
 import math
-def solve(equation_n,xa,xb,n,e):
+def solve(equation_n,xa,xb,n,tolerance):
 	equation_o = equation_n.replace('b','a')
 	equation_sn = equation_n.replace('b','c')
 	for i in range(n):
 		xc = xb - eval(equation_n) * ((xb-xa) / (eval(equation_n) - eval(equation_o)))
 		b = abs((xc - xb) / xb)
 
-		if (b < e):
+		if (b < tolerance):
 			break
 		elif (eval(equation_n) * eval(equation_sn) < 0):
 			temp = xb
@@ -23,10 +23,12 @@ def solve(equation_n,xa,xb,n,e):
 		elif (eval(equation_n) * eval(equation_sn) >= 0):
 			xb = xc
 	   
-		
 	print ("Root of "+equation_n+'= '+str(xc))
-
+	
+	
+#Solving equation x^2 - 2*x -1 by initial values 1,2 iteration = 10, tolerance = 0.001
 solve("xb**2 - 2*xb - 1",1,2,10,0.0001)
+#Solving equation 5*(math.sin(x)^2) - 8*(math.cos(x)^5) by initial values 0.5,1.5 iteration = 10, tolerance = 0.0001
 solve("5*(math.sin(xb)**2) - 8*(math.cos(xb)**5)",0.5,1.5,10,0.00001)
 
 
