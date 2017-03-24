dd12d8c34295d3dfd0b8c348627e569a2b15a104
akjayant <ashish000987@gmail.com>
2017-03-24 12:47:24 +0530

    Update equations.py

--- assignment2_Algorrithm/equations.py
+++ assignment2_Algorrithm/equations.py
@@ -26,7 +26,7 @@ def solve(equation_n,xa,xb,n,tolerance):
 	print ("Root of "+equation_n+'= '+str(xc))
 	
 	
-#Solving equation x^2 - 2*x -1 by initial values 1,2 iteration = 10, tolerance = 0.001
+#Solving equation x^2 - 2*x -1 by initial values 1,2 iteration = 10, tolerance = 0.0001
 solve("xb**2 - 2*xb - 1",1,2,10,0.0001)
 #Solving equation 5*(math.sin(x)^2) - 8*(math.cos(x)^5) by initial values 0.5,1.5 iteration = 10, tolerance = 0.0001
 solve("5*(math.sin(xb)**2) - 8*(math.cos(xb)**5)",0.5,1.5,10,0.00001)
